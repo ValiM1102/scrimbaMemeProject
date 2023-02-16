c4e7fca411af14d76fa478ba897727d01647d810
Valentin <vmihaila333@gmail.com>
2023-02-16 14:42:07 +0200

    random save

--- js/app.js
+++ js/app.js
@@ -1,13 +1,37 @@
 import {catsData} from '/js/data.js'
 
 const emotionRadios = document.querySelector('#emotion-radios')
+const getImageBtn = document.querySelector('#get-image-btn')
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+getImageBtn.addEventListener('click', getMatchingCatsArray)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+
+}
+
+function getMatchingCatsArray(e){
+    const catsArray = []
+    
+}
 
 function getEmotionsArray(cats){
     const emotionsArray = []
 
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            
+            if(!emotionsArray.includes(emotion)){
+              emotionsArray.push(emotion)  
+            }
+            
         }
     }
     return emotionsArray
@@ -17,9 +41,21 @@ function renderEmotionsRadios(cats){
     let radioItems = []
     const emotions = getEmotionsArray(cats)
     for(let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+            <div class='radio'>
+                <label for='${emotion}'>${emotion}</label>
+                <input
+                    type='radio'
+                    id='${emotion}'
+                    value='${emotion}'
+                    name='emotion'
+                >
+            </div>
+            `
     }
-    emotionRadios.innerHTML = radioItems
+        emotionRadios.innerHTML = radioItems
+    
+   
     
 }
 
