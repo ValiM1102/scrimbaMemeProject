db9c343558cc72b8472d34e0c465df73d93fc741
Valentin <vmihaila333@gmail.com>
2023-02-18 11:30:07 +0200

    finished on my own

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ import {catsData} from '/js/data.js'
 
 const emotionRadios = document.querySelector('#emotion-radios')
 const getImageBtn = document.querySelector('#get-image-btn')
+const gifsOnlyOption = document.querySelector('#gifs-only-option')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
-getImageBtn.addEventListener('click', getMatchingCatsArray)
+getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
@@ -17,10 +18,52 @@ function highlightCheckedOption(e){
 
 }
 
+
+
 function getMatchingCatsArray(e){
-    const catsArray = []
+
+    if (document.querySelector('input[type="radio"]:checked')){
+        const selectedCat = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+
+        const matchingCatEmotion = catsData.filter(function(cat){
+            if(isGif){
+                return cat.isGif && cat.emotionTags.includes(selectedCat)
+            }else{
+                return cat.emotionTags.includes(selectedCat)
+            }
+        })
+       return matchingCatEmotion
+    }
     
 }
+
+
+function getSingleCat(){
+   const catsArray = getMatchingCatsArray()
+   if(catsArray.length === 1){
+    return catsArray[0]
+   }else{
+    const randomNumber = Math.floor(Math.random() * catsArray.length)
+    return catsArray[randomNumber]
+}
+}
+function renderCat(){
+    const cat = getSingleCat()
+    //modal appears here
+    const memeModal = document.querySelector('#meme-modal')
+    memeModal.style.display = 'flex'
+    //introducing cat meme to the modal
+    const memeModalInner = document.querySelector('#meme-modal-inner')
+    memeModalInner.innerHTML = `<img class="cat-img" src="images/${cat.image}" alt="${cat.alt}">`
+    //button to close the modal
+    const memeModalCloseBtn = document.querySelector('#meme-modal-close-btn')
+    memeModalCloseBtn.addEventListener('click', function(){
+        memeModal.style.display = 'none'
+    })
+}
+
+
 
 function getEmotionsArray(cats){
     const emotionsArray = []
