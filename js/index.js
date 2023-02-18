de940f45c82383565ab1dfcc1afc1d5e01e2901d
Valentin <vmihaila333@gmail.com>
2023-02-18 12:48:08 +0200

    done

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ import {catsData} from '../js/data.js'
 const emotionRadios = document.querySelector('#emotion-radios')
 const getImageBtn = document.querySelector('#get-image-btn')
 const gifsOnlyOption = document.querySelector('#gifs-only-option')
+const memeModal = document.querySelector('#meme-modal')
+const memeModalInner = document.querySelector('#meme-modal-inner')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 getImageBtn.addEventListener('click', renderCat)
@@ -18,7 +20,28 @@ function highlightCheckedOption(e){
 
 }
 
+function renderCat(){
+    const cat = getSingleCat()
+    //introducing cat meme to the modal
+    memeModalInner.innerHTML = `<img class="cat-img" src="images/${cat.image}" alt="${cat.alt}">`
+    //modal appears here
+    memeModal.style.display = 'flex'
+    //button to close the modal
+    const memeModalCloseBtn = document.querySelector('#meme-modal-close-btn')
+    memeModalCloseBtn.addEventListener('click', function(){
+        memeModal.style.display = 'none'
+    })
+}
 
+function getSingleCat(){
+    const catsArray = getMatchingCatsArray()
+    if(catsArray.length === 1){
+     return catsArray[0]
+    }else{
+     const randomNumber = Math.floor(Math.random() * catsArray.length)
+     return catsArray[randomNumber]
+ }
+ }
 
 function getMatchingCatsArray(e){
 
@@ -37,33 +60,6 @@ function getMatchingCatsArray(e){
     }
     
 }
-
-
-function getSingleCat(){
-   const catsArray = getMatchingCatsArray()
-   if(catsArray.length === 1){
-    return catsArray[0]
-   }else{
-    const randomNumber = Math.floor(Math.random() * catsArray.length)
-    return catsArray[randomNumber]
-}
-}
-function renderCat(){
-    const cat = getSingleCat()
-    //modal appears here
-    const memeModal = document.querySelector('#meme-modal')
-    memeModal.style.display = 'flex'
-    //introducing cat meme to the modal
-    const memeModalInner = document.querySelector('#meme-modal-inner')
-    memeModalInner.innerHTML = `<img class="cat-img" src="images/${cat.image}" alt="${cat.alt}">`
-    //button to close the modal
-    const memeModalCloseBtn = document.querySelector('#meme-modal-close-btn')
-    memeModalCloseBtn.addEventListener('click', function(){
-        memeModal.style.display = 'none'
-    })
-}
-
-
 
 function getEmotionsArray(cats){
     const emotionsArray = []
@@ -97,9 +93,7 @@ function renderEmotionsRadios(cats){
             `
     }
         emotionRadios.innerHTML = radioItems
-    
-   
-    
+       
 }
 
 renderEmotionsRadios(catsData)
